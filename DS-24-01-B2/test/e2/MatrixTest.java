a67bcc6141fb7fa4a5c4d9d368fce1fc782f3c18
AlejandroDopico2 <alejandro.dopico2@udc.es>
2020-11-20 11:59:54 +0100

    E2

--- DS-24-01-B2/test/e2/MatrixTest.java
+++ DS-24-01-B2/test/e2/MatrixTest.java
@@ -5,26 +5,73 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MatrixTest {
+    public static int[][] m1 = {{0,0,0},
+                                {0,0,0},
+                                {0,0,0}};
 
-    //@Test
-    //void rowColumnConstructor(){
-    //    Matrix m = new Matrix(0,0);
-    //    assertEquals(0, m.getRow());
-    //    assertEquals(0, m.getColumn());
+    public static int[][] m2 = {{1}};
+
+    public static int[][] m3
+            = {{1, 2},
+            {3, 4}};
+
+    public static int[][] m4
+            = {{1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}};
+
+    @Test
+    void rowColumnConstructor(){
+        Matrix m = new Matrix(1,1);
+        assertEquals(1, m.getRow());
+        assertEquals(1, m.getColumn());
 
 
-   //     m = new Matrix(2,2);
-   //     assertEquals(2, m.getRow());
-    //    assertEquals(2, m.getColumn());
+        m = new Matrix(3,3);
+        assertEquals(3, m.getRow());
+        assertEquals(3, m.getColumn());
+        for(int i = 0; i < 3; i ++){
+            for(int j = 0; j < 3; j++){
+                assertEquals(0, m.getValor(i, j));
+            }
+        }
 
-    //    m = new Matrix(4,1);
-    //    assertEquals(4, m.getRow());
-    //    assertEquals(1, m.getColumn());
+        m = new Matrix(4,1);
+        assertEquals(4, m.getRow());
+        assertEquals(1, m.getColumn());
 
-    //    m = new Matrix(5,5);
-    //    assertEquals(5, m.getRow());
-     //   assertEquals(5, m.getColumn());
+        m = new Matrix(1,4);
+        assertEquals(1, m.getRow());
+        assertEquals(4, m.getColumn());
+        
+        assertThrows(IllegalArgumentException.class, () -> new Matrix(0, 0));
+        assertThrows(IllegalArgumentException.class, () -> new Matrix(-1, -1));
+        assertThrows(IllegalArgumentException.class, () -> new Matrix(2, -1));
+        assertThrows(IllegalArgumentException.class, () -> new Matrix(-1, 2));
 
-     //   assertThrows(IllegalArgumentException.class, () -> new Matrix(-1, -1));
-    //}
+        m = new Matrix(m2);
+        for(int i = 0; i < m.getRow(); i ++){
+            for(int j = 0; j < m.getColumn(); j++){
+                assertEquals(m2[i][j], m.getValor(i, j));
+            }
+        }
+
+        int[][] mNoRectangular = {{0, 1, 2},
+                                  {3, 4},
+                                  {5, 6, 7}};
+        assertThrows(IllegalArgumentException.class, () -> new Matrix(mNoRectangular));
+
+        int[][] mNoRectangular2 =   {{0, 1},
+                                    {2, 3, 4},
+                                    {5, 6, 7}};
+        assertThrows(IllegalArgumentException.class, () -> new Matrix(mNoRectangular2));
+
+        int[][] mNoRectangular3 =   {{0, 1},
+                                    {2, 3},
+                                    {4}};
+        assertThrows(IllegalArgumentException.class, () -> new Matrix(mNoRectangular3));
+
+        
+
+    }
 }
