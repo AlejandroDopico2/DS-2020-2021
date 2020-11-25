97e16b39036a6a748301514992186e92e5b3ae30
AlejandroDopico2 <alejandro.dopico2@udc.es>
2020-11-26 00:22:24 +0100

    Test1 casi hecho

--- DS-24-01-B2/src/e1/Orcos.java
+++ DS-24-01-B2/src/e1/Orcos.java
@@ -7,8 +7,9 @@ public class Orcos extends Bestia{
 
     @Override
     public void Ataque(Dado dado, Personaje rival){
-        int armor=0;
         int dmg = dado.Tirada(90);
+        int armor = rival.getArmor();
+        armor *= 0.9;
 
         rival.Damage(dmg);
     }
