1cc1234c297dcb741135dab876e2a925dee53a38
AleajandroDopico2 <alejandro.dopico2@udc.es>
2020-12-11 18:16:11 +0100

    Un poco de 1

---
+++ DS-24-01-PD/src/e1/Modos.java
@@ -0,0 +1,8 @@
+package e1;
+
+public enum Modos {
+    OFF,
+    MANUAL,
+    TIMER,
+    PROGRAM;
+}
--- DS-24-01-PD/src/e1/Termostato.java
+++ DS-24-01-PD/src/e1/Termostato.java
@@ -1,6 +1,21 @@
 package e1;
 
 public class Termostato {
+    private boolean on;
+    private float temperature;
+    private int time;
 
+    private void newTemperature (float currentTemperature){
+        time += 5;
+
+        //if( modo == timer && time >= timer ){
+            //  modo = off
+            // on = false;
+        if (!on)
+            throw new IllegalArgumentException("El termostato está apagado");
+        else
+            this.temperature = currentTemperature;
+
+    }
 
 }
